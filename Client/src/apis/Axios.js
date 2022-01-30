2653efa675068348fb117b79218c4e65f34e1b06
Minh <dackheros@gmail.com>
2022-01-30 14:18:45 +0700

    Axios path changed

--- Client/src/apis/Axios.js
+++ Client/src/apis/Axios.js
@@ -1,36 +1,35 @@
-import axios from 'axios';
-
+import axios from 'axios'
 export const postAPI = async (url, data, token) => {
-    const res = await axios.post(`/api/${url}`, data, {
+    const res = await axios.post(`nodejs-dev-up.herokuapp.com/api/${url}`, data, {
         headers: { Authorization: token}
-    });
-    return res;
+    })
+    return res
 }
 
 export const getAPI = async (url, token) => {
-    const res = await axios.get(`/api/${url}`, {
+    const res = await axios.get(`nodejs-dev-up.herokuapp.com/api/${url}`, {
         headers: {Authorization: token}
-    });
-    return res;
+    })
+    return res
 }
 
 export const putAPI = async (url, data, token) => {
-    const res = await axios.put(`/api/${url}`, data, {
+    const res = await axios.put(`nodejs-dev-up.herokuapp.com/api/${url}`, data, {
         headers: {Authorization: token}
-    });
-    return res;
+    })
+    return res
 }
 
 export const patchAPI = async (url, data, token) => {
-    const res = await axios.patch(`/api/${url}`, data, {
+    const res = await axios.patch(`nodejs-dev-up.herokuapp.com/api/${url}`, data, {
         headers: {Authorization: token}
-    });
-    return res;
+    })
+    return res
 }
 
 export const deleteAPI = async (url, token) => {
-    const res = await axios.delete(`/api/${url}`, {
+    const res = await axios.delete(`nodejs-dev-up.herokuapp.com/api/${url}`, {
         headers: {Authorization: token}
-    });
-    return res;
+    })
+    return res
 }
--- Client/src/apis/Cloudinary.js
+++ Client/src/apis/Cloudinary.js
@@ -1,30 +1,26 @@
 export const upload = async (images) => {
-    let imgArr = [];
+    let imgArr = []
     for(const i of images){
-        const formData = new FormData();
-        formData.append("file", i);
-        formData.append("cloud_name", "minh2027");
+        const formData = new FormData()
+        formData.append("file", i)
+        formData.append("cloud_name", "minh2027")
         formData.append("upload_preset", "xf9mllsp")
 
         const res = await fetch("https://api.cloudinary.com/v1_1/minh2027/upload", {
             method: "POST",
             body: formData
-        });
+        })
 
-        const data = await res.json();
-        imgArr.push({public_id: data.public_id, url: data.secure_url});
+        const data = await res.json()
+        imgArr.push({public_id: data.public_id, url: data.secure_url})
     }
-    return imgArr;
-}
-
-export const remove = async () => {
-    
+    return imgArr
 }
 
 export const checkImage = (image) => {
-    let err;
-    if(!image) return err='Image not found!';
-    if(image.size > 1024*1024*15) return err='Image is larger than 15mb';
-    if(image.type !== 'image/png' && image.type !== 'image/jpeg' && image.type !== 'image/jpg') return err='Format is not supported';
-    return err;
+    let err
+    if(!image) return err='Image not found!'
+    if(image.size > 1024*1024*15) return err='Image is larger than 15mb'
+    if(image.type !== 'image/png' && image.type !== 'image/jpeg' && image.type !== 'image/jpg') return err='Format is not supported'
+    return err
 }
