a013b226b31429c0775887bdbbea69c67262a8a1
Minh <dackheros@gmail.com>
2022-01-30 13:44:42 +0700

    Commit change

---
+++ Client/app.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const path = require('path')
+
+const app = express()
+
+const PORT = process.env.PORT || 3000
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('build'))
+    app.get('*', (req, res) => {
+        req.sendFile(path.join(__dirname, 'build', 'index.html'))
+    })
+}
+
+app.listen(PORT, err=>{
+    if(err) return console.error(err)
+    console.log("Server started at " + PORT)
+})
